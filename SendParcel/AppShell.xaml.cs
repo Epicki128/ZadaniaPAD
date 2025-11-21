3b1f0b4c206c6d836d9e06cfc24d7780896b40ff
Epicki128 <epicki128@proton.me>
2025-11-22 00:23:05 +0100

    Finished asigment "Send Parcel Form"

---
+++ SendParcel/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SendParcel
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SendParcel/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SendParcel
+{
+    public partial class MainPage : ContentPage
+    {
+        private int parcelType = 1;
+        private string[,] types = { { "pocztowka.png", "Cena: 1zł" }, { "list.png", "Cena: 1,50zł" }, { "paczka.png", "Cena: 10zł" } };
+        private string postalCodeTemplate = @"^\d{2}-\d{3}$";
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void setParcelType(object sender, CheckedChangedEventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+            parcelType = int.Parse(rb.Value?.ToString());
+        }
+
+        private void getParcelType(object sender, EventArgs e)
+        {
+            Icon.Source = ImageSource.FromFile(types[parcelType, 0]);
+            Price.Text = types[parcelType, 1];
+        }
+
+        private void Submit(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch($"{EntPostalCode.Text}", postalCodeTemplate)) DisplayAlert("Zatwierdzono!", "Dane przesyłki zostały wprowadzone", "OK");
+            else DisplayAlert("Wystąpił błąd!", "Kod pocztowy nie jest poprawny", "OK");
+        }
+    }
+}
