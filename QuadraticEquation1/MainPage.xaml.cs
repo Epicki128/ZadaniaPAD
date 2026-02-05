29c1cb593691362571073776fcea4d3b4f47342a
Epicki128 <epicki128@proton.me>
2026-02-05 23:42:33 +0100

    Finished Assignment Quadratic Equation - Part 1 & 2

---
+++ QuadraticEquation1/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace QuadraticEquation1
+{
+    public partial class MainPage : ContentPage
+    {
+        private string baseResult = "y = 3x^2 + 2x - 2";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculate(object sender, EventArgs e)
+        {
+            double x;
+            double.TryParse(EntryNumber.Text.ToString(), out x);
+
+            string result = baseResult;
+
+            result += $"\ny = 3*{Math.Pow(x, 2)} + {2 * x} - 2";
+            result += $"\ny = {3 * Math.Pow(x, 2)} + {(2 * x) - 2}";
+            result += $"\ny = {(3 * Math.Pow(x, 2)) + (2 * x) - 2}";
+
+            LabelResult.Text = result;
+        }
+    }
+}
