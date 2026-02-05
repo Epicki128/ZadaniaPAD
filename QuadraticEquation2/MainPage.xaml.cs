2e9b0e0b7ec1ad4b46d79b20bb28ebd0815ed7ed
Epicki128 <epicki128@proton.me>
2026-02-06 00:05:05 +0100

    Finished Assignment "Quadratic Equation - Part 2"

---
+++ QuadraticEquation2/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace QuadraticEquation2
+{
+    public partial class MainPage : ContentPage
+    {
+        private string baseResult = "y = ax^2 + bx + c";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculate(object sender, EventArgs e)
+        {
+            double x, a, b, c;
+            double.TryParse(EntryX.Text.ToString(), out x);
+            double.TryParse(EntryA.Text.ToString(), out a);
+            double.TryParse(EntryB.Text.ToString(), out b);
+            double.TryParse(EntryC.Text.ToString(), out c);
+
+            string result = baseResult;
+
+            result += $"\ny = {a}*{x}^2 + {b}*{x} {(c >= 0 ? "+ " + c : c)}";
+            result += $"\ny = {a}*{Math.Pow(x, 2)} + {b * x} {(c >= 0 ? "+ " + c : c)}";
+            result += $"\ny = {a * Math.Pow(x, 2)} + {(b * x) + c}";
+            result += $"\ny = {(a * Math.Pow(x, 2)) + (b * x) - c}";
+
+            LabelResult.Text = result;
+        }
+    }
+}
---
+++ QuadraticEquation2/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace QuadraticEquation2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
